977aa855c3157e34e5bc5b1dfbcad09c1403a48d
Edmir Čolo <coloedmir@gmail.com>
2024-01-15 01:57:30 +0100

    clean_code

--- lib/_all.dart
+++ lib/_all.dart
@@ -1,39 +1,18 @@
 //Dart
 export 'dart:async';
-
-// export 'package:abstract_bloc/abstract_bloc.dart' hide RefreshIndicator, RefreshIndicatorState;
-// export 'package:firebase_core/firebase_core.dart';
-// export 'package:firebase_messaging/firebase_messaging.dart';
-//Flutter
 export 'package:flutter/material.dart' hide Intent;
 export 'package:flutter/services.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
-// export 'package:google_sign_in/google_sign_in.dart';
-// export 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
-// export 'package:sign_in_with_apple/sign_in_with_apple.dart';
-// export 'package:gap/gap.dart';
-// export 'package:get_it/get_it.dart';
-// export 'package:intl/intl.dart' hide TextDirection;
 export 'package:rest_api_client/implementations/rest_api_client.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:catcher/catcher.dart';
 export 'package:cached_network_image/cached_network_image.dart';
 export 'package:go_router/go_router.dart';
 export 'package:gap/gap.dart';
-//Packages
-// import 'package:permission_handler/permission_handler.dart';
-// import 'package:rest_api_client/rest_api_client.dart';
-// export 'package:storage_repository/storage_repository.dart';
-// export './config/firebase_options.dart';
-// export './config/flavors.dart';
-//App
-// export 'app/_all.dart';
 export 'config/_all.dart';
 export 'domain/_all.dart';
-// export 'extensions/_all.dart';
 export 'service_providers/_all.dart';
 export 'presentation/_all.dart';
 export 'blocs/_all.dart';
 export 'l10n/_all.dart';
-// export 'startup/_all.dart';
